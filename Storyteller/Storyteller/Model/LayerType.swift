9e9f44532e52b7c464ebed8191a5c1457710c422
Tian Fang <tianfangsg@gmail.com>
2021-03-21 18:09:03 +0800

    Make LayerType Codable

--- Storyteller/Storyteller/Model/LayerType.swift
+++ Storyteller/Storyteller/Model/LayerType.swift
@@ -5,9 +5,15 @@
 //  Created by TFang on 20/3/21.
 //
 
-import Foundation
-
-enum LayerType: Codable {
+enum LayerType: String {
     case drawing
     case image
+}
+
+// MARK: Hashable
+extension LayerType: Hashable {
+}
+
+// MARK: Codable
+extension LayerType: Codable {
 }
