e204c20a2dae06e947d25cee1fa2b14f43786977
Pan Yongjing <yjpan47@gmail.com>
2021-03-21 10:08:03 +0800

    Scenes and Projects

---
+++ Storyteller/Storyteller/ViewController+View/Scenes/ShotCell.swift
@@ -0,0 +1,40 @@
+//
+//  ShotView.swift
+//  Storyteller
+//
+//  Created by John Pan on 21/3/21.
+//
+
+import UIKit
+
+class ShotCell: UICollectionViewCell {
+    static let identifier = "ShotCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "shot")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.addSubview(self.imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+//        self.imageView.image = nil
+    }
+    
+}
